fd5e0b194e5efd79da781113dba8e20e6b30ee7c
theeurbanlegend <mbuto152@gmail.com>
2023-06-05 20:53:06 +0300

    Printing sum and comments

--- main.js
+++ main.js
@@ -4,15 +4,21 @@ let isAlive=true
 let hasBlackJack=false
 let message=""
 let sum=firstCard+secondCard
-console.log(sum)
-if (sum<=20){
-    message="Do you want to draw a new card?"
-}else if(sum===21){
-    message="Woohoo! Youve got blackjack"
-    hasBlackJack=true
-}else{
-    message="Sorry! You're out of the game"
-    isAlive=false
-}
+let messageEl=document.getElementById("message-el")
+let sumEl=document.getElementById("sum-el")
 
-console.log(message)
+function startGame(){
+
+    if (sum<=20){
+        message="Do you want to draw a new card?"
+    }else if(sum===21){
+        message="Woohoo! Youve got blackjack"
+        hasBlackJack=true
+    }else{
+        message="Sorry! You're out of the game"
+        isAlive=false
+    }
+    messageEl.textContent=message
+    sumEl.innerText+=" "
+    sumEl.innerText+=sum
+}
