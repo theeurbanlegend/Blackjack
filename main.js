c3559e46b339a28d71e5b70e9aba950e80a3c95b
theeurbanlegend <mbuto152@gmail.com>
2023-06-06 16:14:44 +0300

    Adding logic to the game

--- main.js
+++ main.js
@@ -1,13 +1,46 @@
-let firstCard=11
-let secondCard=2
-let isAlive=true
+let player={
+    name:"Player1",
+    chips:"200"
+}
+let cards=[]
+let sum=0
+let isAlive=false
 let hasBlackJack=false
 let message=""
-let sum=firstCard+secondCard
 let messageEl=document.getElementById("message-el")
 let sumEl=document.getElementById("sum-el")
 let cardsEl=document.getElementById("cards-el")
+let playerEL=document.getElementById("player-el")
+playerEL.textContent=player.name + ": $"
+playerEL.textContent+=player.chips
+
+
+function getRandomCard(){
+    let random=(Math.floor(Math.random()*13)+1)
+    if (random===11||random===12||random===13){
+        return 10
+    }else if(random===1){
+        return 11 
+    }else {
+        return random
+    }
+}
 function startGame(){
+    isAlive=true
+    let firstCard=getRandomCard()  
+    let secondCard=getRandomCard()
+    cards.push(firstCard)
+    cards.push(secondCard) 
+    sum=firstCard+secondCard
+    initGame()
+}
+function initGame(){
+    cardsEl.textContent="Cards: "
+    for (let i=0;i<cards.length;i++){
+        cardsEl.textContent+=cards[i]+ " "
+    }
+    
+    sumEl.textContent= "Sum: "+sum
 
     if (sum<=20){
         message="Do you want to draw a new card?"
@@ -19,10 +52,23 @@ function startGame(){
         isAlive=false
     }
     messageEl.textContent=message
-    cardsEl.innerText="Cards: "+" "+firstCard+" "+secondCard
-    sumEl.innerText= "Sum: "+sum
+   
 }
 
 function newCard(){
+    if(isAlive===true&&hasBlackJack===false){
+    let card=getRandomCard()
+    sum+=card
+    cards.push(card)
+    initGame()
+    }
+    
+    else{
+        message="You already lost! Please restart"
+        messageEl.textContent=message
+        alert("Please refresh the page to restart the game")
+    }
+    
     
 }
+
